a7cda593241b032354d7aceacf688124b3b2973d
Matthew <Matirix@live.com>
2022-04-30 16:51:43 -0700

    Added more CSS and search functionality

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,23 +1,16 @@
+// Name
+inputtype = null;
 function get_pokemon_f() {
+    $('.stats').show();
+    $('#history').show();
     console.log("working from get_pokemon_f")
     pokemon = $("#poke_name").val();
-    
-    $.ajax(
-        {
-            "url":`https://pokeapi.co/api/v2/pokemon/${pokemon}`,
-            "type": "GET",
-            "success": display
-        }
-    )
-}
-
-function get_pokemon_home(pokename) {
-    console.log("working from get_pokemon_f")
-    // pokemon1 = $("#poke_name").val();
+    inputtype = $('input[name=select]:checked', '#radiobuttons').val()
+    console.log(inputtype)
 
     $.ajax(
         {
-            "url":`https://pokeapi.co/api/v2/pokemon/${pokename}`,
+            "url":`https://pokeapi.co/api/v2/${inputtype}/${pokemon}`,
             "type": "GET",
             "success": display
         }
@@ -25,6 +18,74 @@ function get_pokemon_home(pokename) {
 }
 
 
+function get_pokemon_history(){
+    //URL
+    console.log($("#historydd option:selected").val())
+    x = ($("#historydd option:selected").val())
+    $.ajax(
+        {
+            "url": `https://pokeapi.co/api/v2/pokemon/${x}`,
+            "type": "GET",
+            "success": display
+        }
+    )
+}
+
+function display(data) {
+    console.log(inputtype + "from display")
+    if (inputtype == "type") {
+        $("#right").hide();
+        console.log("Type is successful")
+        names = []
+        for (i = 0; i != data.pokemon.length; i++) {
+            names.push(data.pokemon[i].pokemon.name + "<br>")
+        }
+
+        $("#picture").html(names);
+
+    } else {
+        picture = (data['sprites']['other']['official-artwork']['front_default']);
+
+        // For the abilities
+        $("#abilities").html("");
+        for (i=0; i != data.abilities.length; i++) {
+            skills = data.abilities[i].ability.name
+            $("#abilities").append("<li>" + skills + "</li>") 
+
+        }
+
+
+
+        $("#historydd").append(`<option value=${data.species.name}>` + data.species.name + "</option>");
+        $("#pkname").html(data.species.name);
+        $("#type").html(data['types'][0]['type']['name']);
+        $("#picture").html(`<img src=${picture}>`);
+        $(".weight").html("Weight: " + data.weight);
+
+    }
+
+
+
+}
+
+
+// --------------------------------------------
+
+//Homepage
+// function get_pokemon_home(pokename) {
+//     console.log("working from get_pokemon_f")
+//     // pokemon1 = $("#poke_name").val();
+
+//     $.ajax(
+//         {
+//             "url":`https://pokeapi.co/api/v2/pokemon/${pokename}`,
+//             "type": "GET",
+//             "success": display
+//         }
+//     )
+// }
+
+//Homepage
 random_poke_index =[]
 function random_nums() {
     // Random numbers
@@ -45,12 +106,8 @@ function random_nums() {
                 "success": parsed_poke_list
             }
         )
-
     }
-    // console.log("Random_nums succ")
-    // console.log(random_poke_list)
 }
-
 
 function parsed_poke_list (rpoke) {
     // console.log(rpoke['height']);
@@ -68,16 +125,7 @@ function reveal(child) {
 
 
 
-function display(data) {
-    console.log("display")
-    console.log(data)
-    picture = (data['sprites']['other']['official-artwork']['front_default']);
-    $("#name").html(data.species.name);
-    $("#type").html(data['types'][0]['type']['name']);
-    $("#picture").html(`<img src=${picture}>`);
-    $("#abilities").html(JSON.stringify(data['abilities']))
 
-}
 
 
 
@@ -88,6 +136,10 @@ function setup() {
     random_nums();
     console.log(random_poke_list);
     $('#get_pokemon').click(get_pokemon_f);
+    $('#poke_history').on('click', get_pokemon_history)
+    $('.stats').hide();
+    $('#history').hide();
+
 
 
 
